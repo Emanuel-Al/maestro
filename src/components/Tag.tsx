c6624d8e389b285726abc18bdf1918a5f41419b8
Emanuel Reino <emanuelreino13@gmail.com>
2025-10-06 09:45:13 -0300

    feat: tag component

---
+++ src/components/Tag.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import clsx from "clsx";
+
+type Status = "WANT_TO_LEARN" | "LEARNING" | "PRACTICING" | "LEARNT";
+
+interface TagProps {
+  status: Status;
+}
+
+const Tag = ({ status }: TagProps) => {
+  return (
+    <div className="flex">
+      <h3
+        className={clsx(
+          `${
+            status === "WANT_TO_LEARN" &&
+            "text-[#4B556C] bg-gray-200 rounded-2xl w-auto"
+          } ${
+            status === "LEARNING" &&
+            "text-[#4863EC] bg-[#DBEAFE] rounded-2xl w-auto"
+          } ${
+            status === "PRACTICING" &&
+            "text-[#ED5813] bg-[#FFEDD5] rounded-2xl w-auto"
+          }
+           ${
+             status === "LEARNT" &&
+             "text-[#42A34A] bg-[#DCFCE7] rounded-2xl w-auto"
+           }`
+        )}
+      >
+        {status === `WANT_TO_LEARN` && "Quero aprender"}
+        {status === `LEARNING` && "Aprendendo"}
+        {status === `PRACTICING` && "Praticando"}
+        {status === `LEARNT` && "Aprendida"}
+      </h3>
+    </div>
+  );
+};
+
+export default Tag;
