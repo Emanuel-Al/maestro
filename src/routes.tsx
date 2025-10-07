1ce9f82219c8664261c9bb4f485d87a29f721b1a
Emanuel Reino <emanuelreino13@gmail.com>
2025-10-07 10:11:22 -0300

    refactor: Route now uses layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Songs from "./pages/Songs";
+import Layout from "./components/layouts/Layout";
 
 const RouterProvider = () => {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route caseSensitive path="/" element={<Home />} />
-        <Route caseSensitive path="/songs" element={<Songs />} />
+        <Route path="/" element={<Layout />}>
+          <Route caseSensitive index element={<Home />} />
+          <Route caseSensitive path="songs" element={<Songs />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
