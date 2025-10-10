c62d615bffaddf47afc4aa4b9129244a57cc7ba5
Emanuel Reino <emanuelreino13@gmail.com>
2025-10-10 20:08:13 -0300

    feat: FilterBtn component

---
+++ src/components/FilterBtn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { IoFunnelOutline } from "react-icons/io5";
+
+const FilterBtn = () => {
+  return (
+    <button className="p-2 rounded-b-md shadow-sm text-center flex gap-2 bg-white items-center font-semibold hover:bg-gray-50 cursor-pointer">
+      <IoFunnelOutline /> Filtros
+    </button>
+  );
+};
+
+export default FilterBtn;
