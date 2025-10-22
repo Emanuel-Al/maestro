c74ac304aff7b790965d9624029ac1951a3f60d1
Emanuel Reino <emanuelreino13@gmail.com>
2025-10-22 14:02:25 -0300

    feat: getBandOptions api

---
+++ src/api/band.ts
@@ -0,0 +1,20 @@
+import { API_URL } from "./config";
+
+export async function getBandOptions(){
+    const url = `${API_URL}/bands`
+    try{
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+        if(!response.ok){
+            throw new Error("Erro ao pegar bandas");
+        }
+        const data = await response.json();
+        return data.map((band: {name:string}) => band.name);
+    }catch(e){
+        console.log(e);
+    }
+}
