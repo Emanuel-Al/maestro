af802e454fcb28f3aecffcdb21fe15efcd347f4f
Emanuel Reino <emanuelreino13@gmail.com>
2025-10-22 14:02:44 -0300

    feat: getTuningOptions api

---
+++ src/api/tuning.ts
@@ -0,0 +1,20 @@
+import { API_URL } from "./config";
+
+export const getTuningOptions = async () => {
+    const url = `${API_URL}/tuning`
+    try{
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+        if(!response.ok){
+            throw new Error("Erro ao retornar afinações");
+        }
+        const data = await response.json();
+        return data.map((tuning:{name: string}) => tuning.name)
+    }catch(e){
+        console.log(e);
+    }
+}
