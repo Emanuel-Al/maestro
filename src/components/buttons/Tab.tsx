1f5bff828785b2ad71bc0375148d30ce9f32f0f5
Emanuel Reino <emanuelreino13@gmail.com>
2025-11-17 20:27:20 -0300

    refactor: removing useless div

--- src/components/buttons/Tab.tsx
+++ src/components/buttons/Tab.tsx
@@ -8,18 +8,16 @@ interface TabProps {
 
 const Tab = ({ isActive, page, onClick }: TabProps) => {
   return (
-    <div>
-      <button
-        onClick={onClick}
-        className={
-          isActive
-            ? "bg-[#3478F6] p-1.5 text-white rounded-2xl min-w-24 cursor-pointer hover:opacity-70 shadow-sm dark:bg-[#1E1E1E] dark:bg-[#3478F6]"
-            : "bg-white p-1.5 text-gray-400 rounded-2xl cursor-pointer hover:opacity-70 shadow-sm dark:bg-[#1E1E1E]"
-        }
-      >
-        {page}
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      className={
+        isActive
+          ? "bg-[#3478F6] p-1.5 text-white rounded-2xl min-w-24 cursor-pointer hover:opacity-70 shadow-sm"
+          : "bg-white p-1.5 text-gray-400 rounded-2xl cursor-pointer hover:opacity-70 shadow-sm dark:bg-[#1E1E1E]"
+      }
+    >
+      {page}
+    </button>
   );
 };
 
