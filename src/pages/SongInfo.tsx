d275ce396f620272ca0e503de39694b40655e450
Emanuel Reino <emanuelreino13@gmail.com>
2025-11-25 17:46:53 -0300

    feat: conecting SongInfo to API

--- src/pages/SongInfo.tsx
+++ src/pages/SongInfo.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoChevronBack } from "react-icons/io5";
-import type { SongDataInterface } from "../interfaces/SongDataInterface";
+import { getSongById, updateSong } from "../api/songs";
 import type { SelectChangeEvent } from "@mui/material";
+import { useParams } from "react-router";
 import {
   TextField,
   InputLabel,
@@ -13,269 +14,210 @@ import Textarea from "@mui/joy/Textarea";
 import FormBtn from "../components/buttons/FormBtn";
 import { SongStatusValues, type SongStatus } from "../enums/SongStatus";
 
-const SongInfo = ({
-  name,
-  band,
-  album,
-  tuning,
-  status,
-  description,
-}: SongDataInterface) => {
+const SongInfo = () => {
   const isDarkMode = document.documentElement.classList.contains("dark");
+  const { id } = useParams();
+  const songId = Number(id);
+
+  const [formData, setFormData] = useState<any>(null);
+
+  useEffect(() => {
+    async function loadData() {
+      const data = await getSongById(songId);
+      setFormData({
+        name: data.name,
+        album: data.album,
+        status: data.status,
+        description: data.description,
+        band: { name: data.band.name },
+        tuning: { name: data.tuning.name },
+      });
+    }
+    loadData();
+  }, [songId]);
+
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    const { id, value } = e.target;
-    setFormData((prev) => ({ ...prev, [id.replace("-input", "")]: value }));
+    const { name, value } = e.target;
+
+    if (name === "band") {
+      setFormData((prev: any) => ({
+        ...prev,
+        band: { ...prev.band, name: value },
+      }));
+      return;
+    }
+
+    if (name === "tuning") {
+      setFormData((prev: any) => ({
+        ...prev,
+        tuning: { ...prev.tuning, name: value },
+      }));
+      return;
+    }
+
+    setFormData((prev: any) => ({ ...prev, [name]: value }));
   };
+
   const handleSelectChange = (e: SelectChangeEvent<SongStatus>) => {
-    setFormData((prev) => ({
+    setFormData((prev: any) => ({
       ...prev,
       status: e.target.value as SongStatus,
     }));
   };
-  const [formData, setFormData] = useState({
-    name,
-    band,
-    album,
-    tuning,
-    status,
-    description,
-  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await updateSong(songId, {
+      name: formData.name,
+      album: formData.album,
+      description: formData.description,
+      status: formData.status,
+      band: { name: formData.band.name },
+      tuning: { name: formData.tuning.name },
+    });
+
+    console.log("Atualizado com sucesso!");
+  };
+
+  if (!formData) return null;
 
   return (
-    <form className="p-6">
+    <form className="p-6" onSubmit={handleSubmit}>
       <header className="flex gap-5 items-center">
-        <div>
-          <button>
-            <IoChevronBack size={25} />
-          </button>
-        </div>
-        <div className="text-3xl font-bold mb-4">
-          <h1>{name}</h1>
-        </div>
+        <button type="button">
+          <IoChevronBack size={25} />
+        </button>
+
+        <h1 className="text-3xl font-bold mb-4">{formData.name}</h1>
       </header>
+
       <main className="bg-white p-5">
-        <div className="">
-          {/*SONG INPUT*/}
-          <div>
-            <InputLabel
-              sx={{
-                color: isDarkMode ? "#fff" : "#6B7280",
-              }}
-              id="song-input"
-            >
-              Música
-            </InputLabel>
-            <TextField
-              fullWidth
-              placeholder="Música"
-              id="song-input"
-              value={name}
-              onChange={handleChange}
-              sx={{
-                input: { color: isDarkMode ? "#fff" : "#111827" },
-                label: { color: isDarkMode ? "#E5E7EB" : "#6B7280" },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: isDarkMode ? "#9CA3AF" : "#9CA3AF",
-                  },
-                },
-              }}
-            />
-          </div>
-          <div>
-            {/*BAND INPUT*/}
-            <InputLabel
-              sx={{
-                color: isDarkMode ? "#fff" : "#6B7280",
-              }}
-              id="band-input"
-            >
-              Banda
-            </InputLabel>
-            <TextField
-              fullWidth
-              placeholder="Banda"
-              id="band-input"
-              value={band}
-              onChange={handleChange}
-              sx={{
-                input: { color: isDarkMode ? "#fff" : "#111827" },
-                label: { color: isDarkMode ? "#E5E7EB" : "#6B7280" },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: isDarkMode ? "#9CA3AF" : "#9CA3AF",
-                  },
-                },
-              }}
-            />
-          </div>
+        <div className="space-y-3">
+          {/* SONG */}
+          <InputLabel sx={{ color: isDarkMode ? "#fff" : "#6B7280" }}>
+            Música
+          </InputLabel>
+          <TextField
+            fullWidth
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            sx={{
+              input: { color: isDarkMode ? "#fff" : "#111827" },
+              "& fieldset": {
+                borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
+              },
+            }}
+          />
+
+          {/* BAND */}
+          <InputLabel sx={{ color: isDarkMode ? "#fff" : "#6B7280" }}>
+            Banda
+          </InputLabel>
+          <TextField
+            fullWidth
+            name="band"
+            value={formData.band.name}
+            onChange={handleChange}
+            sx={{
+              input: { color: isDarkMode ? "#fff" : "#111827" },
+              "& fieldset": {
+                borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
+              },
+            }}
+          />
+
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {/* ALBUM */}
             <div>
-              {/*ALBUM INPUT*/}
-              <InputLabel
-                sx={{
-                  color: isDarkMode ? "#fff" : "#6B7280",
-                }}
-                id="album-input"
-              >
-                Album
+              <InputLabel sx={{ color: isDarkMode ? "#fff" : "#6B7280" }}>
+                Álbum
               </InputLabel>
               <TextField
                 fullWidth
-                id="album-input"
-                placeholder="Álbum"
-                value={album}
+                name="album"
+                value={formData.album}
                 onChange={handleChange}
                 sx={{
                   input: { color: isDarkMode ? "#fff" : "#111827" },
-                  label: { color: isDarkMode ? "#E5E7EB" : "#6B7280" },
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
-                    },
-                    "&:hover fieldset": {
-                      borderColor: isDarkMode ? "#9CA3AF" : "#9CA3AF",
-                    },
+                  "& fieldset": {
+                    borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
                   },
                 }}
               />
             </div>
+
+            {/* TUNING */}
             <div>
-              {/*TUNING INPUT*/}
-              <InputLabel
-                sx={{
-                  color: isDarkMode ? "#fff" : "#6B7280",
-                }}
-                id="tuning-input"
-              >
+              <InputLabel sx={{ color: isDarkMode ? "#fff" : "#6B7280" }}>
                 Afinação
               </InputLabel>
               <TextField
                 fullWidth
-                id="tuning-input"
-                value={tuning}
+                name="tuning"
+                value={formData.tuning.name}
                 onChange={handleChange}
                 sx={{
                   input: { color: isDarkMode ? "#fff" : "#111827" },
-                  label: { color: isDarkMode ? "#E5E7EB" : "#6B7280" },
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
-                    },
-                    "&:hover fieldset": {
-                      borderColor: isDarkMode ? "#9CA3AF" : "#9CA3AF",
-                    },
+                  "& fieldset": {
+                    borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
                   },
                 }}
               />
             </div>
           </div>
         </div>
-        <div>
-          {/*STATUS INPUT*/}
-          <InputLabel
+
+        {/* STATUS */}
+        <InputLabel sx={{ color: isDarkMode ? "#fff" : "#6B7280" }}>
+          Status
+        </InputLabel>
+        <FormControl fullWidth>
+          <Select
+            name="status"
+            value={formData.status}
+            onChange={handleSelectChange}
+            required
             sx={{
-              color: isDarkMode ? "#fff" : "#6B7280",
+              color: isDarkMode ? "#fff" : "#111827",
+              "& fieldset": {
+                borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
+              },
             }}
-            id="status-input"
           >
-            Status
-          </InputLabel>
-          <FormControl fullWidth>
-            <InputLabel
-              id="song-status"
-              sx={{
-                color: isDarkMode ? "#fff" : "#6B7280",
-              }}
-            >
-              Status
-            </InputLabel>
-            <Select
-              labelId="song-status"
-              name="status"
-              value={formData.status}
-              onChange={handleSelectChange}
-              required
-              sx={{
-                color: isDarkMode ? "#fff" : "#111827",
-                "& .MuiOutlinedInput-notchedOutline": {
-                  borderColor: isDarkMode ? "#6B7280" : "#D1D5DB",
-                },
-                "&:hover .MuiOutlinedInput-notchedOutline": {
-                  borderColor: isDarkMode ? "#9CA3AF" : "#9CA3AF",
-                },
-              }}
-              renderValue={(selected) => {
-                const labelMap: Record<SongStatus, string> = {
-                  WANT_TO_LEARN: "Quero aprender",
-                  LEARNING: "Aprendendo",
-                  PRACTICING: "Praticando",
-                  LEARNT: "Aprendido",
-                };
-                return labelMap[selected as SongStatus];
-              }}
-              MenuProps={{
-                PaperProps: {
-                  sx: {
-                    bgcolor: isDarkMode ? "#1E1E1E" : "#fff",
-                    "& .MuiMenuItem-root": {
-                      color: isDarkMode ? "#fff" : "#111827",
-                    },
-                    "& .MuiMenuItem-root:hover": {
-                      bgcolor: isDarkMode ? "#2A2A2A" : "#F3F4F6",
-                    },
-                  },
-                },
-              }}
-            >
-              {Object.values(SongStatusValues).map((status) => (
-                <MenuItem key={status} value={status}>
-                  {status === "WANT_TO_LEARN"
-                    ? "Quero aprender"
-                    : status === "LEARNING"
-                    ? "Aprendendo"
-                    : status === "PRACTICING"
-                    ? "Praticando"
-                    : "Aprendido"}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+            {Object.values(SongStatusValues).map((status) => (
+              <MenuItem key={status} value={status}>
+                {status === "WANT_TO_LEARN"
+                  ? "Quero aprender"
+                  : status === "LEARNING"
+                  ? "Aprendendo"
+                  : status === "PRACTICING"
+                  ? "Praticando"
+                  : "Aprendido"}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {/* DESCRIPTION */}
+        <InputLabel sx={{ color: isDarkMode ? "#fff" : "#6B7280" }}>
+          Anotações
+        </InputLabel>
+        <Textarea
+          name="description"
+          minRows={4}
+          value={formData.description}
+          onChange={handleChange}
+        />
+
+        {/* FOOTER */}
+        <div className="flex gap-2 mt-6 justify-center">
+          <FormBtn type="submit">Salvar mudanças</FormBtn>
+          <FormBtn type="button">Cancelar</FormBtn>
         </div>
-        <section>
-          <div>
-            {/*DESCRIPTION INPUT*/}
-            <InputLabel
-              sx={{
-                color: isDarkMode ? "#fff" : "#6B7280",
-              }}
-              id="description-input"
-            >
-              Anotações
-            </InputLabel>
-            <Textarea
-              placeholder="Adicione suas anotações"
-              id="description-input"
-              value={description}
-            />
-          </div>
-          <div className="flex gap-2 mt-5 justify-end">
-            <div>
-              <FormBtn type="submit">Salvar mudanças</FormBtn>
-            </div>
-            <div>
-              <FormBtn type="button">Cancelar</FormBtn>
-            </div>
-          </div>
-        </section>
       </main>
     </form>
   );
